00958225c746d58a337af084cf775553cfeb1473
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-12-01 21:43:08 +0200

    Add specialties table and pivot table for doctor-specialty many-to-many relationship

--- app/Models/Specialty.php
+++ app/Models/Specialty.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ClientDoctor extends Model
+class Specialty extends Model
 {
     use HasFactory;
-    protected $fillable = ['client_id', 'doctor_id'];
+    public function doctors(){
+        return $this->belongsToMany(Specialty::class,'doctor_specialty');
+    }
 }
