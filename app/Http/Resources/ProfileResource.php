e51ab31774e054fc8c7dbba98d10bc58ac539afa
mohamed abdallah <mohamedabdallahh26@gmail.com>
2024-12-02 23:59:27 +0200

    Set up the ProfileResource

---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = [
+            'name' => $this->name,
+            'email' => $this->email,
+            'role' => $this->role,
+            'phone' => $this->phone,
+            'governorate' => $this->governorate
+        ];
+
+        if ($this->role == 'doctor') {
+            $data['bio'] = $this->doctor->bio;
+            $data['experience_year'] = $this->doctor->experience_year;
+            $data['fees'] = $this->doctor->fees;
+        }
+
+        if ($this->role == 'client') {
+            $data['notes'] = $this->client->notes;
+            $data['medical_history'] = $this->client->medical_history;
+        }
+
+        return $data;
+      }
+}
