31bd28b08f57f2a5d273bf070a395dfe4596eaef
mohamed abdallah <mohamedabdallahh26@gmail.com>
2025-03-16 04:13:24 +0200

    added slot start time and end time to bookings table

---
+++ database/migrations/2025_03_15_223656_add_slot_times_to_bookings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->time('slot_start_time')->after('doctor_id');
+            $table->time('slot_end_time')->after('slot_start_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn(['slot_start_time', 'slot_end_time']);
+
+        });
+    }
+};
